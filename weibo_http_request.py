d5de28e1c456027cc8e0fdf27bb9dcdc7ff2c6b0
ling0322 <ling032x@gmail.com>
2013-07-31 21:17:05 +0800

    implement weibo login class and weibo http request class

--- weibo_http_request.py
+++ weibo_http_request.py
@@ -4,6 +4,9 @@
 # ling0322 2013-07-31
 #
 
+import urllib.request
+from io import BytesIO
+import gzip
 
 class WeiboHttpRequest:
     """
@@ -16,19 +19,40 @@ class WeiboHttpRequest:
         content = http_request.get(weibo_url)
     """
 
-    def __init__(self, login):
-        self.__weibo_login = login
+    def __init__(self, weibo_login):
+        self.__cookies = weibo_login.get_cookie()
 
-    def get(url):
+    def __build_header(self):
+        header = dict()
+        header['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        header['Accept-Encoding'] = 'gzip,deflate,sdch'
+        header['Accept-Language'] = 'en-US,en;q=0.8'
+        header['Connection'] = 'keep-alive'
+        header['DNT'] = '1'
+        header['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36'
+        header['Cookie'] = '; '.join(map(lambda t: '{0}={1}'.format(t[0], t[1]), self.__cookies.items()))
+
+        return header
+
+    def get(self, url):
         """
         A GET request to the url and returns the content of that url in str type
-
-        Not implemented !!!
         """
 
-        return ""
+        request = urllib.request.Request(url = url, headers = self.__build_header())
+        fp = urllib.request.urlopen(request)
+        http_message = fp.info()
+        content = fp.read()
+        fp.close()
 
-    def post(url, data):
+        if http_message.get('Content-Encoding') == 'gzip':
+            buf = BytesIO(content)
+            f = gzip.GzipFile(fileobj = buf)
+            content = f.read()
+
+        return content.decode('utf-8')
+
+    def post(self, url, data):
         """
         A POST request to the url and returns the content in str type
 
--- weibo_login.py
+++ weibo_login.py
@@ -4,6 +4,15 @@
 # ling0322 2013-07-31
 #
 
+import urllib.request
+from urllib.parse import urlencode
+import base64
+import rsa
+import re
+import json
+import binascii
+
+
 class WeiboLogin:
     """
     WeiboLogin simulates the weibo login behavior with username and password, and finally 
@@ -12,16 +21,111 @@ class WeiboLogin:
     Usage:
 
         login = WeiboLogin(your_username, your_password)
-        cookies_str = login.get_cookies()
+        cookies = login.get_cookies()
     """
+
+    LOGIN_URL = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.11)'
+    SSOLOGIN_JS_URL = 'http://login.sina.com.cn/sso/prelogin.php?entry=sso&callback=sinaSSOController.preloginCallBack&rsakt=mod&client=ssologin.js(v1.4.11)'
+    PUBKEY = 'EB2A38568661887FA180BDDB5CABD5F21C7BFD59C090CB2D245A87AC253062882729293E5506350508E7F9AA3BB77F4333231490F915F6D63C55FE2F08A49B353F444AD3993CACC02DB784ABBB8E42A9B1BBFFFB38BE18D78E87A0E41B9B8F73A928EE0CCEE1F6739884B9777E4FE9E88A1BBE495927AC4A799B3181D6442443'
+    RSAKV = '1330428213'
+    CALLBACK_URL = 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack'
 
     def __init__(self, username, password):
         self.__username = username
         self.__password = password
 
-    def get_cookies():
+    def __prelogin(self):
         """
-        Not Implemented 
+        prelogin returns server_time and nonce for login crypto
         """
 
-        return "_s_tentry=-; Apache=4143019942566.7524.1375238457760; SINAGLOBAL=4143019942566.7524.1375238457760; ULV=1375238457767:1:1:1:4143019942566.7524.1375238457760:; SUE=es%3D7a48552d3071e5bd379dbc2b027914c4%26ev%3Dv1%26es2%3D4889ae95d6c92e300712f208dc26ca7e%26rs0%3DFtKkhNT3EF9BjL2OGWCwJ4lBDtWU3pXcfArtEdKPBUzZPAyocc%252FZj%252F%252F4LkodAQmCQqzhpL%252FrYA2BUaDpkMvs%252B1aUlnSb2Cc32MKie%252FVMahC1FuROmEr4lgbn6Y3aafl%252BMwV2MwmJO3CYgsr%252BoBXk%252BqHF%252FB1V6f9%252BLhZVDOGaHr4%253D%26rv%3D0; SUP=cv%3D1%26bt%3D1375238478%26et%3D1375324878%26d%3Dc909%26i%3D0660%26us%3D1%26vf%3D0%26vt%3D0%26ac%3D0%26st%3D0%26uid%3D2605372777%26name%3D327716067%2540qq.com%26nick%3DYukiBot%26fmp%3D%26lcp%3D2012-10-16%252011%253A16%253A23; SUS=SID-2605372777-1375238478-XD-hl5t8-c5974593b001f256525011383dc2680a; ALF=1377830477; SSOLoginState=1375238478; wvr=5"
+        fp = urllib.request.urlopen(self.SSOLOGIN_JS_URL)
+        content = fp.read()
+        fp.close()
+
+        content = content.decode('utf-8')
+        json_data = re.search('\((.*)\)', content).group(1)
+        data = json.loads(json_data)
+        server_time = str(data['servertime'])
+        nonce = data['nonce']
+        return server_time, nonce
+
+    def __encrypt_username(self, username):
+        username_quoted = urllib.parse.quote(username)
+        username_encrypted = base64.encodestring(username_quoted.encode('utf-8'))[:-1]
+        return username_encrypted.decode('utf-8')
+
+    def __encrypt_password(self, password, server_time, nonce):
+        rsa_public_key = int(self.PUBKEY, 16)
+        key = rsa.PublicKey(rsa_public_key, 65537)
+        message = str(server_time) + '\t' + str(nonce) + '\n' + str(password)
+        message_bytes = message.encode('utf-8')
+        passwd = rsa.encrypt(message_bytes, key)
+        return binascii.b2a_hex(passwd).decode('utf-8')
+
+    def __build_login_form_data(self, encryptd_username, encrypted_password, server_time, nonce):
+        return {
+            'entry': 'weibo',
+            'gateway': '1',
+            'from': '',
+            'savestate': '7',
+            'userticket': '1',
+            'ssosimplelogin': '1',
+            'vsnf': '1',
+            'vsnval': '',
+            'su': encryptd_username,
+            'service': 'miniblog',
+            'servertime': server_time,
+            'nonce': nonce,
+            'pwencode': 'rsa2',
+            'sp': encrypted_password,
+            'encoding': 'UTF-8',
+            'prelt': '115',
+            'rsakv' : self.RSAKV,
+            'url': self.CALLBACK_URL,
+            'returntype': 'META'
+        }
+
+    def __parse_cookie(self, http_message):
+        cookies_dict = {}
+        for key, value in http_message.items():
+            if key == 'Set-Cookie':
+                cookie_line = value[: value.find(';')]
+                fields = cookie_line.split('=')
+                cookies_dict[fields[0]] = fields[1]
+
+        return cookies_dict
+
+
+
+
+    def get_cookie(self):
+        try:
+            server_time, nonce = self.__prelogin()
+            encryptd_username = self.__encrypt_username(self.__username)
+            encrypted_password = self.__encrypt_password(self.__password, server_time, nonce)
+            form_data = self.__build_login_form_data(encryptd_username, encrypted_password, server_time, nonce)
+            
+            request  = urllib.request.Request(url = self.LOGIN_URL, data = urlencode(form_data).encode('utf-8'))
+            fp = urllib.request.urlopen(request)
+            content = fp.read()
+            fp.close()
+
+            content = content.decode('gbk')
+            match = re.search('location\.replace\(\"(.*?)\"\)', content)
+            callback_url = match.group(1)
+
+            fp = urllib.request.urlopen(callback_url)
+            content = fp.read()
+            cookie_dict = self.__parse_cookie(fp.info())
+            fp.close()
+
+            if 'SUE' in cookie_dict and cookie_dict['SUE'] != 'deleted':
+                return cookie_dict
+        except e:
+            print(e)
+
+        return None
+
+
+
