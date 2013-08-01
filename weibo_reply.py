1ac5c1bb51b6b471eeff54b5b6b75c3b851ecf6c
ling0322 <ling032x@gmail.com>
2013-08-01 20:44:44 +0800

    fix the max_replies not working bug in weibo reply

--- weibo_reply.py
+++ weibo_reply.py
@@ -176,10 +176,11 @@ def get_weibo_reply(http_request, weibo_url, reply_max = 200):
         reply_list = __get_reply_at_page(http_request, mid, page)
         log.log("weibo_reply", "GET {0} replies from page {1}".format(len(reply_list), page))
         weibo_reply.extend(reply_list)
-        if len(reply_list) == 0:
+        count += len(reply_list)
+        if len(reply_list) == 0 or count >= reply_max:
             break
 
-    return weibo_reply
+    return weibo_reply[: 200]
 
 
 
