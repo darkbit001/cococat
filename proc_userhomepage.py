943a2de6738976de3534b19e6de8944c1ae856b6
李蕾 <leiytc@gmail.com>
2014-08-12 11:33:42 +0800

    parse user relation info

--- proc_userhomepage.py
+++ proc_userhomepage.py
@@ -38,14 +38,40 @@ def parse_user_base_infos(dbo_userpages):
 		if pageId == -1:
 			print(pageUrl)
 	return
+def relation_parser(htmlStr):
+	user_dict = {}
+	user_dict['follower_num'] = -1
+	user_dict['followee_num'] = -1
+	user_dict['weibo_num'] = -1
+	pattern_follower_num = re.compile(r'<strong node-type=\\"fans\\">(.+?)<\\/strong>\\r\\n\\t\\t\\t<span>粉丝<\\/span>')
+	pattern_followee_num = re.compile(r'<strong node-type=\\"follow\\">(.+?)<\\/strong>\\r\\n\\t\\t\\t<span>关注 <\\/span>')
+	pattern_weibo_num = re.compile(r'<strong node-type=\\"weibo\\">(.+?)<\\/strong>\\r\\n\\t\\t\\t<span>微博<\\/span>')
+	if pattern_follower_num.search(html_str) != None:
+		user_dict['follower_num'] = pattern_follower_num.findall(html_str)[0]
+	if pattern_followee_num.search(html_str) != None:
+		user_dict['followee_num'] = pattern_followee_num.findall(html_str)[0]
+	if pattern_weibo_num.search(html_str) != None:
+		user_dict['weibo_num'] = pattern_weibo_num.findall(html_str)[0]
+	return user_dict
+	
 def parse_user_relation(dbo_userpages):
-	cursor = dbo_userpages.coll.find({}, {'htmlStr': 1, 'pageUrl': 1})
+	cursor = dbo_userpages.coll.findOne({}, {'htmlStr': 1})
 	for c in cursor:
 		_id = c['_id']
-	pass
+		htmlStr = c['htmlStr']
+		ud = relation_parser(htmlStr)
+		follower_num = ud['follower_num']
+		followee_num = ud['follower_num']
+		weibo_num = ud['weibo_num']
+		print(follower_num)
+		print(followee_num)
+		print(weibo_num)
+		#dbo_userpages.coll.update({'_id': _id} ,{'$set': {'followerNum': follower_num, 'followeeNum': followee_num, 'weiboNum': weibo_num} }, multi = True)
+	return
 def main():
 	dbo = dboperator.Dboperator(collname = 'UserHomePages')
 	parse_user_base_infos(dbo)
+	parse_user_relation(dbo)
 	dbo.connclose()
 
 main()
